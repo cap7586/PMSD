53e10f78437e04d07e2ffad140bfba53ce53f710
cap7586 <carolinapaez996@gmail.com>
2023-12-07 20:18:52 +0100

    Added some comments to the code

--- gas_station/src/my_functions_gas_station.rs
+++ gas_station/src/my_functions_gas_station.rs
@@ -2,11 +2,11 @@
 
 use std::io; //io library comes from the standard library std
 
-////These lines import necessary modules from the standard library
+//These lines import necessary modules from different libraries
 use std::error::Error; //Used for error handling
 use std::fs::File; //Used to work with files
 use std::fs::{OpenOptions, rename};
-use csv::{ReaderBuilder, WriterBuilder};//use csv::{ReaderBuilder, WriterBuilder};
+use csv::{ReaderBuilder, WriterBuilder};
 
 pub enum Operation {
     Sell,
@@ -19,22 +19,9 @@ pub enum Operation {
     Exit,
 }
 
-/*impl Item {
-    // Constructor method to create an Item instance from a StringRecord
-    fn from_record(record: &StringRecord) -> Result<Item, Box<dyn Error>> {
-        Ok(Item {
-            id: record[0].parse()?,
-            name: record[1].to_string(),
-            price: record[2].parse()?,
-            quantity: record[3].parse()?,
-        })
-    }
-
-    // Method to check if the item has a specific ID
-    /*fn has_id(&self, target_id: u32) -> bool {
-        self.id == target_id
-    }*/
-}
+/*
+My first idea was to use a struct, but then I thought it would be better to store all the data in csv files in order to have them still available 
+even after termination of the program execution
 
 #[derive(Clone)] 
 pub struct Item {
@@ -73,20 +60,7 @@ pub fn read_csv(file_path: &str) -> () {
     	println!("{}, {}, {}, {}", id, name, price, quantity);
     }
 }
-/*
-pub fn perform_sell(inventory: &Vec<Item>) {
-    println!("Performing Sell");
-    let item_id: u32 = get_user_input("Enter item ID:") as u32;
-    let mut total_for_item = 0.0;
-    if let Some(item) = inventory.iter().find(|&i| i.id == item_id) {
-        let item_number: f64 = get_user_input("Enter quantity:");
-        total_for_item = item.price * item_number;
-        println!("Total for {}: {}", item.name, total_for_item);
-    } else {
-        println!("Item with ID {} not found in the inventory.", item_id);
-    }
-}
- */
+
 pub fn write_csv_from_user(file_path: &str, id_input: &str, name_input:&str, price_input:&str, quantity_input:&str) -> Result<(), Box<dyn Error>>{
 	//this function is used to actually write new instances (rows) into the csv file that is passed as parameter (file_path)
     
@@ -121,27 +95,16 @@ pub fn add_product_to_inventory(file_path: &str){
     .expect("Failed to write CSV");
 }
 
-/*pub fn display_inventory(inventory: &Vec<Item>) {
-	//this function is printing the inventory for the user into the command line
-    println!("Inventory:");
-    for item in inventory {
-        println!(
-            "ID: {}, Name: {}, Price: {}, Quantity: {}",
-            item.id, item.name, item.price, item.quantity
-        );
-    }
-}*/
-
 pub fn print_menu(){
 	//This function is only printing the menu for the user in the command line
         println!("\n\nSelect the operation you want to perform");
         println!("1. Sell Something");
         println!("2. Change Product Stock");
         println!("3. Add a new Product to the System");
-        println!("4. Reduce Product stock");
+        println!("4. Coming Soon Feature");//not there yet
         println!("5. Update a Product Price");
         println!("6. See Inventory");
-        println!("6b. See One Product");
+        println!("6b. See One Product");//not implemented yet
         println!("7. Delete Product by ID");
         println!("99. Exit\n");
 }
